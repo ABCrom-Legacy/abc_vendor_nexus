198eb6e19f2fc344ddae86e568b92c09a5c13b57
dadi11 <dadi.sikorski@gmail.com>
2017-11-04 14:19:34 +0100

    tools: update changelog script
    * improved the look and readability
    * use %F (2017-12-30) instead %m-%d-%Y (12-30-2017)
    * show only 7 first chars of commit sha
    * remove / at the end of repo names
    * seperate each repo in changelog

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Exports
+dir=$ANDROID_BUILD_TOP
+out=$dir/out/target/product
+
 export Changelog=$PWD/Changelog.txt
 
 if [ -f $Changelog ];
@@ -9,25 +13,32 @@ fi
 
 touch $Changelog
 
-echo "Generating changelog..."
+# Print something to build output
+echo ${bldppl}"Generating changelog..."${txtrst}
 
-for i in $(seq 10);
+for i in $(seq 7);
 do
-export After_Date=`date --date="$i days ago" +%m-%d-%Y`
+export After_Date=`date --date="$i days ago" +%F`
 k=$(expr $i - 1)
-	export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
-
-	# Line with after --- until was too long for a small ListView
-	echo '=======================' >> $Changelog;
-	echo  "     "$Until_Date       >> $Changelog;
-	echo '=======================' >> $Changelog;
-	echo >> $Changelog;
-
-	# Cycle through every repo to find commits between 2 dates
-	repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
-	echo >> $Changelog;
+export Until_Date=`date --date="$k days ago" +%F`
+echo "====================" >> $Changelog;
+echo "     $Until_Date    " >> $Changelog;
+echo "====================" >> $Changelog;
+repo forall -pc 'git log --after=$After_Date --until=$Until_Date --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=7' >> $Changelog
+echo "" >> $Changelog;
 done
 
-sed -i 's/project/   */g' $Changelog
+sed -i 's/project/ */g' $Changelog
+sed -i 's/[/]$//' $Changelog
 
+if [ -e $out/*/$Changelog ]
+then
+rm $out/*/$Changelog
+fi
+if [ -e $out/*/system/etc/$Changelog ]
+then
+rm $out/*/system/etc/$Changelog
+fi
+cp $Changelog $OUT/system/etc/
 cp $Changelog $OUT/
+rm $Changelog
