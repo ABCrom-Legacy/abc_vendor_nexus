cffb8b54f0f42c42cc2a2ad0eaa1512e38381e5f
LuK1337 <priv.luk@gmail.com>
2017-12-22 16:54:41 +0100

    sepolicy: Move fingerprint 2.0 service out of private sepolicy
    Change-Id: Ifb55a009f4ff87b9a3571b24943b5e9c31f9dcd2

--- sepolicy/common/sepolicy.mk
+++ sepolicy/common/sepolicy.mk
@@ -3,5 +3,11 @@
 # inherit from ABC
 #
 
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/nexus/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/nexus/sepolicy/common/private
+
 BOARD_SEPOLICY_DIRS += \
-    vendor/nexus/sepolicy/common
+    vendor/nexus/sepolicy/common/vendor
