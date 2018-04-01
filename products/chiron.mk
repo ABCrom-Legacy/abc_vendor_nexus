b628db21c44ae0bba881e21017e33d8d575dce14
Wight554 <wight554@gmail.com>
2018-04-01 17:06:04 +0000

    products: Add Mi Mix 2

---
+++ products/chiron.mk
@@ -0,0 +1,27 @@
+# Copyright (C) 2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include pure telephony configuration
+include vendor/nexus/configs/abc_phone.mk
+
+# Inherit AOSP device configuration for angler
+$(call inherit-product, device/xiaomi/chiron/abc_chiron.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := chiron
+PRODUCT_DEVICE := chiron
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi MIX 2
+PRODUCT_MANUFACTURER := Xiaomi
+
