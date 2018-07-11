15b1dbba1c505f1dceade7990be41ae43bf24645
Vol Zhdanov <wight554@gmail.com>
2018-07-11 18:28:31 +0000

    products: add sagit
    * blind, will drop if everything is that shit

---
+++ products/sagit.mk
@@ -0,0 +1,26 @@
+# Copyright (C) 2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include pure telephony configuration
+include vendor/nexus/configs/abc_phone.mk
+
+# Inherit AOSP device configuration for angler
+$(call inherit-product, device/xiaomi/sagit/abc_sagit.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := sagit
+PRODUCT_DEVICE := sagit
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 6
+PRODUCT_MANUFACTURER := Xiaomi
